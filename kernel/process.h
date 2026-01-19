25cf2ffc3a3d325479e5e85d048bca8990d60939
Frogskok123 <39944364+Frogskok123@users.noreply.github.com>
2026-01-19 15:19:11 +0500

    Update process.h

--- kernel/process.h
+++ kernel/process.h
@@ -19,40 +19,40 @@
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
 static size_t get_module_base(pid_t pid, char* name)
 {
-\tstruct task_struct* task;
-\tstruct mm_struct* mm;
-\tstruct vm_area_struct *vma;
-\tsize_t count = 0;
-\tchar buf[ARC_PATH_MAX];
-\tchar *path_nm = NULL;
+struct task_struct* task;
+struct mm_struct* mm;
+struct vm_area_struct *vma;
+size_t count = 0;
+char buf[ARC_PATH_MAX];
+char *path_nm = NULL;
 
-\trcu_read_lock();
-\ttask = pid_task(find_vpid(pid), PIDTYPE_PID);
-\tif (!task) {
-\t\trcu_read_unlock();
-\t\treturn 0;
-\t}
-\trcu_read_unlock();
+rcu_read_lock();
+task = pid_task(find_vpid(pid), PIDTYPE_PID);
+if (!task) {
+rcu_read_unlock();
+return 0;
+}
+rcu_read_unlock();
 
-\tmm = get_task_mm(task);
-\tif (!mm) {
-\t\treturn 0;
-\t}
+mm = get_task_mm(task);
+if (!mm) {
+return 0;
+}
     // В новых ядрах итерация VMA может отличаться, здесь оставлена ваша логика
-\tvma = find_vma(mm, 0);
-\twhile (vma) {
-\t\tif (vma->vm_file) {
-\t\t\tpath_nm = d_path(&vma->vm_file->f_path, buf, ARC_PATH_MAX-1);
-\t\t\tif (!IS_ERR(path_nm) && !strcmp(kbasename(path_nm), name)) {
-\t\t\t\tcount = (uintptr_t)vma->vm_start;
-\t\t\t\tbreak;
-\t\t\t}
-\t\t}
-\t\tif (vma->vm_end >= ULONG_MAX) break; 
-\t\tvma = find_vma(mm, vma->vm_end);
-\t}
-\tmmput(mm);
-\treturn count;
+vma = find_vma(mm, 0);
+while (vma) {
+if (vma->vm_file) {
+path_nm = d_path(&vma->vm_file->f_path, buf, ARC_PATH_MAX-1);
+if (!IS_ERR(path_nm) && !strcmp(kbasename(path_nm), name)) {
+count = (uintptr_t)vma->vm_start;
+break;
+}
+}
+if (vma->vm_end >= ULONG_MAX) break; 
+vma = find_vma(mm, vma->vm_end);
+}
+mmput(mm);
+return count;
 }
 #else
 uintptr_t get_module_base(pid_t pid, const char *name)
